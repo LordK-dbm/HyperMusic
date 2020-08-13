7c0cce811b13138363e1c942ff0021608b2ea6a8
LordK-dbm <69652985+LordK-dbm@users.noreply.github.com>
2020-08-13 18:42:50 -0300

    Update and rename index.md to bot.js

---
+++ bot.js
@@ -0,0 +1,23 @@
+## Website suporte HyperMusic
+
+Nosso grupo do discord para suporte: https://discord.gg/F9nvU8E
+
+### Comandos do bot
+
+!play
+!stop
+!skip
+!quit
+!pause
+!resume
+!clearqueue
+!listmusic
+!list
+!repeat
+!volume
+
+### Termos de uso
+
+Comandos do bot: !play, !stop, !skip, !quit, !pause, !resume, !clearqueue, !listmusic, !list, !repeat, !volume. O bot tem um sistema onde e necessario utilizar URL de videos ou 
+musicas do youtuber para reproduzir no canal de voz que você esteja conectado. Caso você digite algum comando onde bug ou trave o bot recomendo que entre em nosso servidor de 
+suporte para estar relatando o erro. O bot foi desenvolvido por LordK#3198. Para saber mais sobre os comandos do bot digite: !help
